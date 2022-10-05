a986dcca2aa8ade7316c3249246e2deb3f285585
Hamish Gamble <hamish.gamble@gmail.com>
2022-10-05 15:15:19 +1030

    Function to calculate sector productivities (output per fte employment)

--- R/sector_productivity.R
+++ R/sector_productivity.R
@@ -1,23 +1,15 @@
-sector_productivity <- function(region) {
+sector_productivity <- function(region, year) {
 
-  industry_productivity <- industry_industry_flows_19 %>%
+  industry_productivity <- create_19_sector() %>%
     tidyr::pivot_longer(cols = 2:length(.),
                  names_to = "anzsic_division_code",
                  values_to = "flow") %>%
-    dplyr::filter(from_anzsic %in% c("Australian Production", "Employed full-time")) %>%
+    dplyr::filter(from_anzsic %in% c("Australian Production", "FTE Employment")) %>%
     tidyr::pivot_wider(names_from = from_anzsic, values_from = flow) %>%
-    dplyr::mutate(productivity = `Australian Production`/`Employed full-time`)
+    dplyr::mutate(productivity = `Australian Production`/`FTE Employment`)
 
-  overall_productivity <- industry_industry_flows_19 %>%
-    tidyr::pivot_longer(cols = 2:length(.),
-                 names_to = "anzsic_division_code",
-                 values_to = "flow") %>%
-    dplyr::filter(from_anzsic %in% c("P1", "P2", "P3", "P4", "Australian Production"),
-           !anzsic_division_code %in% LETTERS)
 
-  #get_region_employment <- function()
-
-  regional_production <- get_regional_employment(region) %>%
+  regional_production <- get_regional_employment(region, year) %>%
     fte_employment() %>%
     dplyr::select(anzsic_division_code, adjust_fte) %>%
     dplyr::left_join(industry_productivity, by = "anzsic_division_code") %>%
