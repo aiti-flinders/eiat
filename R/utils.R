53b801b6bc69b6ffc29af9fe2d697324b990dcf4
Hamish Gamble <hamish.gamble@gmail.com>
2022-10-05 15:14:29 +1030

    Minor updates for consistency

--- R/utils.R
+++ R/utils.R
@@ -1,29 +1,3 @@
-employment_industry_fy <- function(financial_year) {
-
-  fy_left <- strsplit(financial_year, split = "-")[[1]][1]
-  fy_right <- as.character(as.numeric(fy_left) + 1)
-
-  date_left <- as.Date(paste(fy_left, "07-01", sep = "-"))
-  date_right <- as.Date(paste(fy_right, "06-30", sep = "-"))
-
-
-
-  aitidata::employment_by_industry %>%
-    dplyr::filter(dplyr::between(date, date_left, date_right),
-           gender == "Persons", age == "Total (age)",
-           state == "Australia",
-           indicator %in% c("Employed full-time", "Employed total"),
-           industry != "Total (industry)") %>%
-    dplyr::left_join(abscorr::anzsic, by = c("industry" = "anzsic_division")) %>%
-    dplyr::distinct(indicator, anzsic_division_code, value) %>%
-    dplyr::group_by(indicator, anzsic_division_code) %>%
-    dplyr::summarise(value = mean(value)) %>%
-    tidyr::pivot_wider(names_from = anzsic_division_code) %>%
-    dplyr::ungroup() %>%
-    dplyr::rename(from_anzsic = indicator) %>%
-    dplyr::mutate(total_industry_uses = rowSums(across(c(2:20))))
-}
-
 add_non_anzsic_rows <- function(data) {
   data %>%
     add_row(anzsic_division_code = "Total Intermediate Uses", lq = 1) %>%
@@ -39,30 +13,38 @@ add_non_anzsic_rows <- function(data) {
 
 adjust_employment <- function(.data) {
   .data %>%
-    tidyr::pivot_longer(cols = 2:(length(.) - 4),
-                        names_to = "anzsic_division_code",
-                        values_to = "jobs") %>%
-    dplyr::mutate(adjust_inadequate = jobs + (jobs/sum(jobs)) * `Inadequately described`,
-                  adjust_jobs = adjust_inadequate + (adjust_inadequate/sum(adjust_inadequate)) * `Not stated`)
+    tidyr::pivot_wider(names_from = industry,
+                       values_from = employment) %>%
+    tidyr::pivot_longer(cols = -c(lga, year, `Inadequately described`, `Not stated`),
+                        names_to = "industry",
+                        values_to = "employment") %>%
+    dplyr::group_by(lga) %>%
+    dplyr::mutate(adjust_inadequate = employment + (employment/sum(employment)) * `Inadequately described`,
+                  adjust_jobs = adjust_inadequate + (adjust_inadequate/sum(adjust_inadequate)) * `Not stated`) %>%
+    dplyr::filter(industry != "Not applicable") %>%
+    dplyr::ungroup()
+
 
 }
 
 fte_employment <- function(data) {
-  ratio <- industry_industry_flows_19 %>%
-    dplyr::filter(from_anzsic %in% c("Employed full-time", "Employed total")) %>%
+  ratio <- create_19_sector() %>%
+    dplyr::filter(from_anzsic %in% c("FTE Employment", "Total Employment")) %>%
     tidyr::pivot_longer(cols = A:S,
                         names_to = "anzsic_division_code",
                         values_to = "value") %>%
     dplyr::select(anzsic_division_code, from_anzsic, value) %>%
     tidyr::pivot_wider(names_from = from_anzsic,
                        values_from = value) %>%
-    dplyr::mutate(fte = `Employed full-time`/`Employed total`)
+    dplyr::mutate(fte = `FTE Employment`/`Total Employment`) %>%
+    dplyr::left_join(anzsic_swap, by = c("anzsic_division_code" = "letter"))
 
   data %>%
-    dplyr::left_join(ratio, by = "anzsic_division_code") %>%
-    dplyr::mutate(adjust_fte = adjust_jobs * fte) %>%
-    dplyr::select(lga, anzsic_division_code, adjust_fte)
+    dplyr::left_join(ratio, by = c("industry" = "name")) %>%
+    dplyr::mutate(employment = employment * fte) %>%
+    dplyr::select(-c(anzsic_division_code, `FTE Employment`, `Total Employment`, fte))
 }
+
 get_available_regions <- function() {
   local_employment %>%
     dplyr::distinct(usual_residence) %>%
@@ -71,5 +53,10 @@ get_available_regions <- function() {
 }
 
 anzsic_letter_to_number <- function() {
+
+}
+
+ioig_names_number <- function(col_no) {
+  names(io_cols[col_no])
 
 }
