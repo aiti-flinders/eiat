5afe9b7dc1d817864dcf3af96b33147009378979
Hamish Gamble <gamb0043@flinders.edu.au>
2022-10-10 10:11:45 +1030

    Creates LQ models

--- data-raw/create_lq_models.R
+++ data-raw/create_lq_models.R
@@ -4,8 +4,8 @@ devtools::load_all()
 nat_data <- get_data(year = 2016)
 
 lq_models <- get_available_regions(year = 2016) %>%
-  set_names() %>%
-  map(~possibly(rtt_basic, otherwise = "error here")(nat_data, .x))
+  purrr::set_names() %>%
+  purrr::map(~purrr::possibly(rtt_basic, otherwise = "error here")(nat_data, .x))
 
 
 
