6809d876310b0dc33166767730cb52d0aaf6dfee
Hamish Gamble <hamish.gamble@gmail.com>
2022-10-13 09:00:00 +1030

    Closes #6

--- data-raw/io_tables.R
+++ data-raw/io_tables.R
@@ -3,6 +3,8 @@ library(eiat)
 
 industry_flows <- read_industry_flow_table()
 national_employment <- read_national_employment_table()
+national_19 <- create_19_sector()
 
+usethis::use_data(national_19, compress = "xz", overwrite = TRUE)
 usethis::use_data(industry_flows, compress = "xz", overwrite = TRUE)
 usethis::use_data(national_employment, compress = "xz", overwrite = TRUE)
