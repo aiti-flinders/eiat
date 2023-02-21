306413eadeb217f1babc6ca7eb184d28685b2db2
Hamish Gamble <hamish.gamble@flinders.edu.au>
2023-02-21 15:28:07 +1030

    Redownloaded Employment by LGA by POW by Industry by UR 2021 because the previous version had different names for usual residence LGAs and place of work LGAs

--- data-raw/create_lq_models.R
+++ data-raw/create_lq_models.R
@@ -1,4 +1,6 @@
 ## code to prepare `create_lq_models` dataset goes here
+library(dplyr)
+library(purrr)
 devtools::load_all()
 
 nat_data <- get_data(year = 2021)
@@ -7,9 +9,9 @@ create_lq <- function(data, year, type) {
 
 
   lq_models <- get_available_regions(year = {{year}}) %>%
-    dplyr::pull(lga) %>%
-    purrr::set_names() %>%
-    purrr::map(~purrr::possibly(rtt_basic, otherwise = "error here")(data, .x, type))
+    pull(lga) %>%
+    set_names() %>%
+    map(~possibly(rtt_basic, otherwise = "error here")(data, .x, type))
 
   return(lq_models)
 }
@@ -19,5 +21,5 @@ lq_models <- create_lq(nat_data, 2021, "household")
 lq_basic <- create_lq(nat_data, 2021, "basic")
 
 
-usethis::use_data(lq_models, overwrite = TRUE)
-usethis::use_data(lq_basic, overwrite = TRUE)
+usethis::use_data(lq_models, compress = "xz", overwrite = TRUE)
+usethis::use_data(lq_basic, compress = "xz", overwrite = TRUE)
