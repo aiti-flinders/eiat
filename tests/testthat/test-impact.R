36592fbadcfd34b702301684c95b43af6e2bede0
Hamish Gamble <hamish.gamble@flinders.edu.au>
2023-07-26 15:06:51 +0930

    Add tests for reasonableness of impacts

--- tests/testthat/test-impact.R
+++ tests/testthat/test-impact.R
@@ -7,9 +7,11 @@ for (i in seq_along(lq_models)) {
     is_impact <- function(region) {
       out <- impact_analysis(region, impacts)
 
-      x <- !is.na(sum(out$output$value))
-      y <- !is.na(sum(out$emp$value))
-      z <- !is.na(sum(out$grp$value))
+      # Check that there's nothing missing. We can also guess roughly how big the impact should be for a given $1 million to all industries input.
+      # For output, the direct output should equal the input and the total output should be greater than the input
+      x <- !is.na(sum(out$output$value)) & sum(out$output[out$output$type == "Direct Output", "value"]) == 19 & sum(out$output[out$output$type == "Total Output", "value"]) > 19
+      y <- !is.na(sum(out$emp$value)) & sum(out$emp[out$emp$type == "Direct Employment", "value"]) > 0 & sum(out$emp[out$emp$type == "Total Employment", "value"]) > 0
+      z <- !is.na(sum(out$grp$value)) & sum(out$grp[out$grp$type == "Direct GRP","value"]) > 0 & sum(out$grp[out$grp$type == "Total GRP","value"]) > 0
 
       t <- (x & y) & z
 
